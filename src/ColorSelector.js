52377248c17675435dccd58e8d4b21c1096b920e
Kelly <devkells00@gmail.com>
2018-10-05 16:16:21 -0500

    Done:

--- src/ColorSelector.js
+++ src/ColorSelector.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 export default class ColorSelector extends Component {
   
+  //received color as prop. Has fn to update color to the color selected. 
   makeColorSwatches = () => (
     ["#F00", "#F80", "#FF0", "#0F0", "#00F", "#508", "#90D", "#FFF", "#000"].map((str, idx) => {
-      return <div key={idx} className="color-swatch" style={{backgroundColor: str}}/>
+      const updateColor = () => {this.props.selectedColor(str)}
+      return <div key={idx} className="color-swatch" style={{backgroundColor: str}} onClick={updateColor}/>
     })
   )
   
--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,32 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = { color: '#fff'}
   }
 
+  //fn to pass color as prop to Cell
+  currentColor = () => {
+    this.state.color
+  }
+
+  //fn to select new color and set it as state
+  selectedColor = (hex) => {
+    this.setState({color: hex})
+  }
+  
+  //once row is generated, Cell is created and we pass color prop to Cell
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} currentColor={this.currentColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
-
-  render() {
+  render() { 
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector selectedColor={this.selectedColor} /> 
         <div id="matrix">
           {this.genMatrix()}
         </div>
